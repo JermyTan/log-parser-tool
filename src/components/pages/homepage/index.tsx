d7c085a6c414bc118e9b07d68a6e92aade31e35f
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-05-28 07:53:53 +0800

    Add homepage

---
+++ src/components/pages/homepage/index.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Segment,
+  Form,
+  Grid,
+  Icon,
+  Header,
+} from "semantic-ui-react";
+import "./index.scss";
+
+function Homepage() {
+  const [gfsServerUrl, setGfsServerUrl] = useState("");
+  const [gfsFilename, setGfsFilename] = useState("");
+  const [requestorId, setRequestorId] = useState("");
+  const [gfsToken, setGfsToken] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const allFieldsFilled = () =>
+    gfsServerUrl !== "" &&
+    gfsFilename !== "" &&
+    requestorId !== "" &&
+    gfsToken !== "";
+
+  return (
+    <main className="homepage">
+      <Container>
+        <Segment padded raised secondary>
+          <Grid columns="2" relaxed stackable verticalAlign="middle">
+            <Grid.Column>
+              <Form>
+                <Form.Input
+                  label="GFS Server URL"
+                  required
+                  placeholder="https://f.haiserve.com"
+                  value={gfsServerUrl}
+                  onChange={(event, data) => setGfsServerUrl(data.value)}
+                />
+                <Form.Input
+                  label="GFS Filename"
+                  required
+                  placeholder="6ba213f000..."
+                  value={gfsFilename}
+                  onChange={(event, data) => setGfsFilename(data.value)}
+                />
+                <Form.Input
+                  label="Requestor's SeaTalk User ID"
+                  required
+                  placeholder="12345"
+                  value={requestorId}
+                  onChange={(event, data) => setRequestorId(data.value)}
+                />
+                <Form.Input
+                  label="GFS Token"
+                  required
+                  value={gfsToken}
+                  onChange={(event, data) => setGfsToken(data.value)}
+                />
+                <Form.Button
+                  primary
+                  content="Process"
+                  fluid
+                  disabled={!allFieldsFilled()}
+                  loading={loading}
+                  onClick={() => {
+                    setLoading(true);
+                    setTimeout(() => setLoading(false), 1000);
+                  }}
+                />
+              </Form>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <Icon
+                className="homepage-icon"
+                name="file alternate"
+                size="massive"
+              />
+              <Header as="h1">Log Parser Tool</Header>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </Container>
+    </main>
+  );
+}
+
+export default Homepage;
