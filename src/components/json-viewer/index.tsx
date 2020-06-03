38462ff7d7f0e33b7f667d49c94d7b8b935e885c
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-06-03 18:27:11 +0800

    Improve json tree viewer styling

---
+++ src/components/json-viewer/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+import JSONTree from "react-json-tree";
+import "./index.scss";
+
+const theme = {
+  scheme: "monokai",
+  author: "wimer hazenberg (http://www.monokai.nl)",
+  base00: "#272822",
+  base01: "#383830",
+  base02: "#49483e",
+  base03: "#75715e",
+  base04: "#a59f85",
+  base05: "#f8f8f2",
+  base06: "#f5f4f1",
+  base07: "#f9f8f5",
+  base08: "#f92672",
+  base09: "#fd971f",
+  base0A: "#f4bf75",
+  base0B: "#a6e22e",
+  base0C: "#a1efe4",
+  base0D: "#66d9ef",
+  base0E: "#ae81ff",
+  base0F: "#cc6633",
+};
+
+type Props = {
+  data: {} | [any];
+};
+
+function JsonViewer(props: Props) {
+  return (
+    <>
+      {/*<div className="json-viewer-action-buttons">
+        <Button content="Filter" primary compact />
+        <Button content="Reset" color="red" compact />
+      </div>*/}
+      <JSONTree
+        data={props.data}
+        invertTheme={false}
+        theme={{
+          extend: theme,
+          tree: {
+            margin: 0,
+            overflow: "auto",
+            height: "100%",
+            whiteSpace: "nowrap",
+            padding: "0.5rem 0 1rem 1rem",
+          },
+        }}
+        shouldExpandNode={() => true}
+      />
+    </>
+  );
+}
+
+export default JsonViewer;
--- src/components/pages/log-info-page/index.tsx
+++ src/components/pages/log-info-page/index.tsx
@@ -10,29 +10,8 @@ import {
 import { useLogs } from "../../../utils/custom-hooks";
 import { LazyLog } from "../../lazy-log/components";
 import Fullscreen from "react-full-screen";
-import JSONTree from "react-json-tree";
+import JsonViewer from "../../json-viewer";
 import "./index.scss";
-
-const theme = {
-  scheme: "monokai",
-  author: "wimer hazenberg (http://www.monokai.nl)",
-  base00: "#272822",
-  base01: "#383830",
-  base02: "#49483e",
-  base03: "#75715e",
-  base04: "#a59f85",
-  base05: "#f8f8f2",
-  base06: "#f5f4f1",
-  base07: "#f9f8f5",
-  base08: "#f92672",
-  base09: "#fd971f",
-  base0A: "#f4bf75",
-  base0B: "#a6e22e",
-  base0C: "#a1efe4",
-  base0D: "#66d9ef",
-  base0E: "#ae81ff",
-  base0F: "#cc6633",
-};
 
 function LogInfoPage() {
   const [loading, invalid, logs] = useLogs();
@@ -58,14 +37,7 @@ function LogInfoPage() {
                 selectableLines
               />
             ) : (
-              <div className="log-info-json-viewer">
-                <JSONTree
-                  data={value as object}
-                  invertTheme={false}
-                  theme={theme}
-                  shouldExpandNode={() => true}
-                />
-              </div>
+              <JsonViewer data={value as object} />
             )
           ) : (
             <h2 className="log-info-empty-log-label">Empty log file</h2>
